c177b5fc180854fc4f4227f06eeb236eb1698155
Grigorii Fedorenko <g.v.fedorenko@yandex.ru>
2022-10-01 15:33:19 +0300

    test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -113,4 +113,4 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-EMAIL_HOST_USER = 'me@gmail.com' # comment
+EMAIL_HOST_USER = 'me@gmail.com'  # comment
